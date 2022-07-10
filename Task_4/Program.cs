90dcbddee3f391302a07bf0613fe2eb6d5896e9f
Alexey Nechaeff <nechaeff91@gmail.com>
2022-07-10 19:56:42 +0500

    Четвёртое  задание

---
+++ Task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число = ");
+int N = int.Parse(Console.ReadLine());
+int Two = 2;
+
+Console.WriteLine("Хоба");
+while(Two < N)
+{
+    Console.WriteLine(Two);
+    Two = Two + 2;
+}
+
